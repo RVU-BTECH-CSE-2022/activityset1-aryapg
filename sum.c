84da544551530d411f8e7e2c9e978f5a6d1d619a
Arya p g <110161253+aryapg@users.noreply.github.com>
2022-09-09 08:53:27 +0000

    steel.c

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+  printf("enter the two numbers",a,b);
+  scanf("%d",&a);
+  scanf("%d",&b);
+  sum=a+b;
+  printf("sum of two numbers is %d",sum);
+  return 0;
+}
---
+++ world.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+main()
+{
+  printf("Hello world\n");
+  printf("I am C language\n");
+  }
